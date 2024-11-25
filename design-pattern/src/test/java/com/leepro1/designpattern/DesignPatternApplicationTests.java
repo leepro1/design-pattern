f0ce2920ca5e779445a22a2fb63b631a1d4bd328
Huiju Lee <leemoon63@gmail.com>
2024-11-25 23:44:19 +0900

    create design-pattern project

---
+++ design-pattern/src/test/java/com/leepro1/designpattern/DesignPatternApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leepro1.designpattern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignPatternApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

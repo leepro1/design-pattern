a84057cb852ee3d1a3538fff5258edb5683168d1
HuiJu <leemoon63@gmail.com>
2024-12-27 22:25:26 +0900

    Interpreter: design pattern in java

---
+++ design-pattern/src/main/java/com/leepro1/designpattern/interprefer/java/IteratorInSpring.java
@@ -0,0 +1,10 @@
+package com.leepro1.designpattern.interprefer.java;
+
+import org.springframework.util.CompositeIterator;
+
+public class IteratorInSpring {
+
+    public static void main(String[] args) {
+        CompositeIterator iterator;
+    }
+}
